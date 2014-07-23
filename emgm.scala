7c02c4dd0611162f828fa59885e2c02093a46102
Julian Andres Klode <klode@mathematik.uni-marburg.de>
2014-07-23 17:43:55 +0200

    emgm: Make it work :)

--- emgm.scala
+++ emgm.scala
@@ -16,11 +16,13 @@
  * limitations under the License.
  */
 
-/** I'm not sure implementing EMGM is possible in Scala.
+/** Implementation of (Extensible and Modular) Generics for the Masses
   *
-  * The whole thing seems to fail because we cannot have static methods
-  * in a trait. So we cannot access representations of sum or product
-  * elements.
+  * Based on: Extensible and Modular Generics for the Masses
+  * by: Bruno C. d. S. Oliveira , Ralf Hinze, Andres L
+  *
+  * Based on: Scala for Generic Programmers
+  * by: Bruno C. d. S. Oliveira and Jeremy Gibbons
   */
 object EMGM {
   /** Isomorphism for converting between types */
@@ -37,88 +39,90 @@ object EMGM {
     def constr[A](name: Symbol, arity: Int, arg: G[A]): G[A] = arg
     def char: G[Char]
     def int: G[Int]
-    def view[A, B](iso: Iso[B, A], a: G[A]): G[B]
+    def view[A, B](iso: Iso[B, A], a: ⇒ G[A]): G[B]
   }
 
-  /* ====================================================================
-   *          EXAMPLE: Encoding data to bits
-   * ====================================================================
-   */
-  def const[A, B](a: A)(b: B) = a
-  case class Encode[A](encodeS: A ⇒ List[Boolean]) extends Generic[Encode] {
-    override def unit = Encode(const(List()))
-    override def plus[A, B](a: Encode[A], b: Encode[B]) = Encode((x: Either[A, B]) ⇒ x match {
-      case Left(l)  ⇒ false :: a.encodeS(l)
-      case Right(r) ⇒ true :: b.encodeS(r)
-    })
-    override def prod[A, B](a: Encode[A], b: Encode[B]) = Encode((x: (A, B)) ⇒ a.encodeS(x._1) ++ b.encodeS(x._2))
-
-    override def char = Encode(encodeChar)
-    override def int = Encode(encodeInt)
-    override def view[A, B](iso: Iso[B, A], a: Encode[A]) = Encode((x: B) ⇒ a.encodeS(iso.from(x)))
-  }
-  val encodeG = Encode(const(List()))
-
-  /* Stubs */
-  def encodeInt(i: Int) = List(true)
-  def encodeChar(c: Char) = List(false)
-
-  /* ====================================================================
-   *          EXAMPLE: LISTS
-   * ====================================================================
-   */
-  def isoList[A]: Iso[List[A], Either[Unit, (A, List[A])]] = Iso(fromList, toList)
-  def fromList[A](list: List[A]): Either[Unit, (A, List[A])] = list match {
-    case Nil       ⇒ Left(Unit)
-    case (a :: as) ⇒ Right((a, as))
-  }
-
-  def toList[A](list: Either[Unit, (A, List[A])]): List[A] = list match {
-    case Left(())       ⇒ List.empty
-    case Right((a, as)) ⇒ a :: as
-  }
-
-  def rList[G[_], A, B](g: Generic[G])(a: G[A]): G[List[A]] = g.view(isoList, g.plus(g.unit, g.prod(a, rList(g)(a))))
-
-  /** It seems we need to translate Rep like this, but:
+  /** Representation of types.
     *
-    * We cannot implement uct or Eithers this way as we do not have access
-    * to the Reps of the left and right sides.
+    * This gets ugly because we need to pass implicit Rep[A] objects around
+    * for every object A.
     */
   trait Rep[A] {
     def rep[G[_]](g: Generic[G]): G[A]
   }
 
-  /** Let's try implementing Rep like this:
-    *
-    * Does not work either. While we can implement the product case now,
-    * there's no way for us to implement Either, because we can only access
-    * either the left or the right side, and thus, only their representations.
-    */
-  trait Rep1 {
-    def rep1[G[_]](g: Generic[G]): G[this.type]
-  }
-
-  implicit case object RUnit extends Rep[Unit] {
+  implicit def RUnit = new Rep[Unit] {
     override def rep[G[_]](g: Generic[G]): G[Unit] = g.unit
   }
-  implicit case object RChar extends Rep[Char] {
+  implicit def RChar = new Rep[Char] {
     override def rep[G[_]](g: Generic[G]): G[Char] = g.char
   }
-  implicit case object RInt extends Rep[Int] {
+  implicit def RInt = new Rep[Int] {
     override def rep[G[_]](g: Generic[G]): G[Int] = g.int
   }
 
-  /*
-   * We'd need to access a Rep[A] and a Rep[B] for this to work. How are
-   * we going to do this?
-   *
-  case class REither[A, B]() extends Rep[Either[A, B]] {
-    override def rep[G[_]](g: Generic[G]): G[Either[A, B]] = g.plus(rep(g): G[A], rep(g): G[B])
+  /** Thanks to Scala for Generic Programmers :). I did not figure out how to
+    * get the Rep[A] and Rep[B] stuff done, I tried to use case classes for
+    * implementing the Rep, which failed :(
+    */
+  implicit def REither[A, B](implicit a: Rep[A], b: Rep[B]) = new Rep[Either[A, B]] {
+    override def rep[G[_]](g: Generic[G]): G[Either[A, B]] = g.plus(a.rep(g), b.rep(g))
   }
-  */
-  def encode[T](t: T)(implicit r: Rep[T]): List[Boolean] = r.rep(encodeG).encodeS(t)
+  implicit def RTuple2[A, B](implicit a: Rep[A], b: Rep[B]) = new Rep[(A, B)] {
+    override def rep[G[_]](g: Generic[G]): G[(A, B)] = g.prod(a.rep(g), b.rep(g))
+  }
 
-  val b = encodeG.prod(encodeG.int, encodeG.char).encodeS((1, 'x'))
+  object Examples {
 
+    /* ====================================================================
+   *          EXAMPLE: Encoding data to bits
+   * ====================================================================
+   */
+    def const[A, B](a: A)(b: B) = a
+    case class Encode[A](encodeS: A ⇒ List[Boolean]) extends Generic[Encode] {
+      override def unit = Encode(const(List()))
+      override def plus[A, B](a: Encode[A], b: Encode[B]) = Encode((x: Either[A, B]) ⇒ x match {
+        case Left(l)  ⇒ false :: a.encodeS(l)
+        case Right(r) ⇒ true :: b.encodeS(r)
+      })
+      override def prod[A, B](a: Encode[A], b: Encode[B]) = Encode((x: (A, B)) ⇒ a.encodeS(x._1) ++ b.encodeS(x._2))
+
+      override def char = Encode(encodeChar)
+      override def int = Encode(encodeInt)
+      override def view[A, B](iso: Iso[B, A], a: ⇒ Encode[A]) = Encode((x: B) ⇒ a.encodeS(iso.from(x)))
+    }
+    val encodeG = Encode(const(List()))
+
+    /* Stubs */
+    def encodeInt(i: Int) = List(true)
+    def encodeChar(c: Char) = List(false)
+
+    /* Generic function */
+    def encode[T](t: T)(implicit r: Rep[T]): List[Boolean] = r.rep(encodeG).encodeS(t)
+
+    /* ====================================================================
+   *          EXAMPLE: LISTS
+   * ====================================================================
+   */
+    def isoList[A]: Iso[List[A], Either[Unit, (A, List[A])]] = Iso(fromList, toList)
+    def fromList[A](list: List[A]): Either[Unit, (A, List[A])] = list match {
+      case Nil       ⇒ Left(Unit)
+      case (a :: as) ⇒ Right((a, as))
+    }
+
+    def toList[A](list: Either[Unit, (A, List[A])]): List[A] = list match {
+      case Left(())       ⇒ List.empty
+      case Right((a, as)) ⇒ a :: as
+    }
+
+    def rList[G[_], A](a: G[A])(implicit g: Generic[G]): G[List[A]] = {
+      import g._
+      view(isoList, plus(unit, prod(a, rList(a))))
+    }
+
+    implicit def RList[A](implicit a: Rep[A]) = new Rep[List[A]] {
+      override def rep[G[_]](gen: Generic[G]) =
+        rList(a.rep(gen))(gen)
+    }
+  }
 }
