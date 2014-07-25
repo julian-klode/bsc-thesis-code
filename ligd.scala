10437199e74a7a6d5ba75918c43956320a3a6df2
Julian Andres Klode <klode@mathematik.uni-marburg.de>
2014-07-25 16:00:52 +0200

    ligd: Make Reps implicit

--- ligd.scala
+++ ligd.scala
@@ -31,14 +31,14 @@ import org.scalatest._
   */
 object LIGD {
 
-  def geq[A](rep: Rep[A], a: A, b: A): Boolean = (rep, a, b) match {
+  def geq[A](a: A, b: A)(implicit rep: Rep[A]): Boolean = (rep, a, b) match {
     case (RUnit, (), ())                      ⇒ true
-    case (RSum(ra, rb), Left(a1), Left(a2))   ⇒ geq(ra, a1, a2)
-    case (RSum(ra, rb), Right(b1), Right(b2)) ⇒ geq(rb, b1, b2)
+    case (RSum(ra, rb), Left(a1), Left(a2))   ⇒ geq(a1, a2)(ra)
+    case (RSum(ra, rb), Right(b1), Right(b2)) ⇒ geq(b1, b2)(rb)
     case (RSum(_, _), _, _)                   ⇒ false
     case (RProd(ra, rb), (a1, b1), (a2, b2)) ⇒
-      geq(ra, a1, a2) && geq(rb, b1, b2)
-    case (r: RType[_, A], t1, t2) ⇒ geq(r.a, r.b.from(t1), r.b.from(t2))
+      geq(a1, a2)(ra) && geq(b1, b2)(rb)
+    case (r: RType[_, A], t1, t2) ⇒ geq(r.b.from(t1), r.b.from(t2))(r.a)
     case _                        ⇒ false
 
   }
@@ -51,11 +51,14 @@ object LIGD {
    */
   sealed abstract class Rep[+T]
 
-  case object RUnit extends Rep[Unit]
+  implicit case object RUnit extends Rep[Unit]
 
   /** Represent sums */
   case class RSum[A, B](val a: Rep[A], val b: Rep[B]) extends Rep[Either[A, B]]
   case class RProd[A, B](val a: Rep[A], val b: Rep[B]) extends Rep[(A, B)]
+
+  implicit def rSum[A, B](implicit a: Rep[A], b: Rep[B]): Rep[Either[A, B]] = RSum(a, b)
+  implicit def rProd[A, B](implicit a: Rep[A], b: Rep[B]): Rep[(A, B)] = RProd(a, b)
 
   /** Represent any type */
   sealed class RType[C, B](c: ⇒ Rep[C], ep: EP[B, C]) extends Rep[B] {
@@ -84,7 +87,7 @@ object LIGD {
     case Right((a, as)) ⇒ a :: as
   }
 
-  def rList[A](ra: Rep[A]): Rep[List[A]] = RType(
+  implicit def rList[A](implicit ra: Rep[A]): Rep[List[A]] = RType(
     RSum(RUnit, RProd(ra, rList(ra))),
     EP(fromList[A], toList[A])
   )
@@ -94,16 +97,20 @@ object LIGD {
   */
 class LIGDTests extends FlatSpec {
   import LIGD._
+  val unit = ()
   "geq" should "work" in {
-    assert(geq(RUnit, (), ()))
+    assert(geq(unit, unit))
   }
 
+
   "geq" should "support empty lists" in {
-    assert(geq(rList(RUnit), Nil, Nil))
+    assert(geq(List.empty : List[Unit], List.empty))
   }
+
   "geq" should "support non-empty lists" in {
-    assert(geq(rList(rList(RUnit)), List(Nil, Nil), List(Nil, Nil)))
-    assert(!geq(rList(rList(RUnit)), List(Nil), List(Nil, Nil)))
-    assert(!geq(rList(rList(RUnit)), List(Nil), Nil))
+    assert(geq(List(unit, unit), List(unit, unit)))
+    assert(!geq(List(unit), List(unit, unit)))
   }
+
+
 }
